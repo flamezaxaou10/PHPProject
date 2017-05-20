b9550f3c6b800476eb95479a70a3e33cc602bc1d
yume045 <yume045@hotmail.com>
2017-05-20 21:23:52 +0700

    ดูดิเฟรม

---
+++ Login.php
@@ -0,0 +1,36 @@
+<?php include 'header.php' ?>
+<div class="container">
+    <div class="col s12 edge">
+			<h4 class="">Log In</h4>
+			</div>
+      <div class="row">
+        <div class="col s12 ">
+          <div class="input-field col s4">
+            <input name="Username" type="text" class="validate">
+            <label for="Username">Username</label>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 ">
+            <div class="input-field col s4">
+              <input name="Password" type="text" class="validate">
+              <label for="Password">Password</label>
+          </div>
+        </div>
+        <div class="col s12 ">
+        <div class="row">
+          <a class="waves-effect light-blue btn-large darken-4">Submit</a>
+          <label for="remember" style="display: inline;">
+									<span style="font-size: 16px; color: #4d4d4d;">Stay logged in</span>
+					</label>
+        </div>
+      </div>
+      <div class="col s12 ">
+        <div class="input-field col s4">
+        <div class="row">
+          <a name="forgot_password" href="#" class="wave-effect light-blue btn right grey" tabindex="-1">Forgot Password?</a>
+        </div>
+        </div>
+      </div>
+    </div>
