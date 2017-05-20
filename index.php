4e481739359c2b68999bf4002be483738004aa9d
HashtagF <flamezaxaou1@gmail.com>
2017-05-20 20:14:43 +0700

    up

--- index.php
+++ index.php
@@ -7,28 +7,28 @@
       <div class="slider">
         <ul class="slides">
           <li>
-            <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
+            <img src="img/900*450.jpg"> <!-- random image -->
             <div class="caption center-align">
               <h3>This is our big Tagline!</h3>
               <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
             </div>
           </li>
           <li>
-            <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
+            <img src="img/900*450.jpg"> <!-- random image -->
             <div class="caption left-align">
               <h3>Left Aligned Caption</h3>
               <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
             </div>
           </li>
           <li>
-            <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
+            <img src="img/900*450.jpg"> <!-- random image -->
             <div class="caption right-align">
               <h3>Right Aligned Caption</h3>
               <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
             </div>
           </li>
           <li>
-            <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
+            <img src="img/900*450.jpg"> <!-- random image -->
             <div class="caption center-align">
               <h3>This is our big Tagline!</h3>
               <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
@@ -37,6 +37,10 @@
         </ul>
       </div>
     </div>
+    <div class="col s3 "><p>s12 m4</p></div>
+    <div class="col s3"><p>s12 m4</p></div>
+    <div class="col s3"><p>s12 m4</p></div>
+    <div class="col s3"><p>s12 m4</p></div>
     <div class="col s3 "><p>s12 m4</p></div>
     <div class="col s3"><p>s12 m4</p></div>
     <div class="col s3"><p>s12 m4</p></div>
