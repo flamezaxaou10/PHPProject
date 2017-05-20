0cfa5a22d48b9aa81f7345d208cd524cf39dac33
HashtagF <flamezaxaou1@gmail.com>
2017-05-20 21:39:59 +0700

    center login

--- Login.php
+++ Login.php
@@ -1,36 +1,63 @@
 <?php include 'header.php' ?>
-<div class="container">
+<div class="container center-align">
     <div class="col s12 edge">
-			<h4 class="">Log In</h4>
+			<h4 class="">Login</h4>
 			</div>
-      <div class="row">
+      <div class="row center-align">
+        <div class="s12">
+
+        </div>
+      </div>
+      <div class="row center-align">
         <div class="col s12 ">
+          <div class="col s4">
+
+          </div>
           <div class="input-field col s4">
             <input name="Username" type="text" class="validate">
             <label for="Username">Username</label>
+          </div>
+        </div>
+        </div>
+        <div class="row center-align">
+          <div class="col s12 ">
+            <div class="col s4">
+
+            </div>
+            <div class="input-field col s4">
+              <input name="Password" type="text" class="validate">
+              <label for="Password">Password</label>
             </div>
           </div>
         </div>
         <div class="row">
           <div class="col s12 ">
-            <div class="input-field col s4">
-              <input name="Password" type="text" class="validate">
-              <label for="Password">Password</label>
+            <div class="col s4">
+
+            </div>
+            <div class="col s4">
+              <div class="col s6">
+                <a class="waves-effect light-blue btn-large darken-4">Login</a>
+              </div>
+              <div class="col s6">
+                <br>
+                <input type="checkbox" class="filled-in" id="filled-in-box" />
+                <label for="filled-in-box">Stay Login</label>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="col s12 ">
         <div class="row">
-          <a class="waves-effect light-blue btn-large darken-4">Submit</a>
-          <label for="remember" style="display: inline;">
-									<span style="font-size: 16px; color: #4d4d4d;">Stay logged in</span>
-					</label>
+          <div class="col s12">
+            <div class="col s4">
+
+            </div>
+            <div class="input-field col s4">
+              <a name="forgot_password" href="#" class="wave-effect light-blue btn right grey" tabindex="-1">Forgot Password?</a>
+            </div>
+            <div class="col s4">
+
+            </div>
         </div>
       </div>
-      <div class="col s12 ">
-        <div class="input-field col s4">
-        <div class="row">
-          <a name="forgot_password" href="#" class="wave-effect light-blue btn right grey" tabindex="-1">Forgot Password?</a>
-        </div>
-        </div>
-      </div>
-    </div>
+</div>
