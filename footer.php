20bee5d1e2e1be728e163e4b0814a77e44b264c6
HashtagF <flamezaxaou1@gmail.com>
2017-05-20 20:32:38 +0700

    footer

--- footer.php
+++ footer.php
@@ -1,17 +1,19 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <footer class="page-footer #01579b light-blue darken-4">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 class="white-text">บริการลูกค้า</h5>
+                <p class="grey-text text-lighten-4">COM-OUT คือ ร้านค้าออนไลน์ที่ จัดส่งสินค้ารวดเร็ว เชื่อถือได้ และมอบความสะดวกสบายในการเลือกซื้อสินค้าแก่ลูกค้า
+                                                  ด้วยการนำเสนอสินค้ามากมายหลากหลายชนิด พวกเราพร้อมมอบสิ่งที่ดีที่สุด ให้แก่ลูกค้าเสมอ ไม่ว่าจะเป็นวิธีการชำระเงินปลายทาง หรือบริการการคืนสินค้าฟรี และศูนย์บริการลูกค้าที่พร้อมจะให้คำแนะนำตลอดการเลือกซื้อสินค้า</p>
               </div>
               <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
+                <h5 class="white-text">Contact</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><i class="material-icons">call</i><a class="grey-text text-lighten-3" href="#!">&nbsp;&nbsp;&nbsp; +66 XX-XXXX-XXX</a></li>
+                  <li><i class="fa fa-facebook" style="font-size:24px; color : #2196f3;"></i><a class="grey-text text-lighten-3" href="#!">  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;COM-OUT Official</a></li>
+                  <li><i class="fa fa-twitter" style="font-size:24px; color : #2196f3;"></i><a class="grey-text text-lighten-3" href="#!">  &nbsp;&nbsp;&nbsp;&nbsp;COM-OUT Official</a></li>
+                  <li><i class="material-icons" style="font-size:24px; color : #2196f3;">email</i><a class="grey-text text-lighten-3" href="#!">  &nbsp;&nbsp;&nbsp;&nbsp;support@com-out.com </a></li>
                 </ul>
               </div>
             </div>
