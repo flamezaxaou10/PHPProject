bd2fa0dcfaee6cb2b9495bf19e07abdf0a23baea
HashtagF <flamezaxaou1@gmail.com>
2017-05-20 22:21:13 +0700

    พอละสัส

--- index.php
+++ index.php
@@ -7,31 +7,31 @@
       <div class="slider">
         <ul class="slides">
           <li>
-            <img src="img/900x450.jpg"> <!-- random image -->
+            <img clss="responsive-img" src="img/p1.jpg"> <!-- random image -->
             <div class="caption center-align">
-              <h3 class="black-text">This is our big Tagline!</h3>
-              <h5 class="light black-text text-lighten-3">Here's our small slogan.</h5>
+              <h3 class="white-text">This is our big Tagline!</h3>
+              <h5 class="light white-text text-lighten-3">Here's our small slogan.</h5>
             </div>
           </li>
           <li>
-            <img src="img/900x450.jpg"> <!-- random image -->
+            <img clss="responsive-img" src="img/p2.jpg"> <!-- random image -->
             <div class="caption left-align">
-              <h3 class="black-text">Left Aligned Caption</h3>
-              <h5 class="light black-text text-lighten-3">Here's our small slogan.</h5>
+              <h3 class="white-text">Left Aligned Caption</h3>
+              <h5 class="light white-text text-lighten-3">Here's our small slogan.</h5>
             </div>
           </li>
           <li>
-            <img src="img/900x450.jpg"> <!-- random image -->
+            <img clss="responsive-img" src="img/p3.jpg"> <!-- random image -->
             <div class="caption right-align">
-              <h3 class="black-text">Right Aligned Caption</h3>
-              <h5 class="light black-text text-lighten-3">Here's our small slogan.</h5>
+              <h3 class="white-text">Right Aligned Caption</h3>
+              <h5 class="light white-text text-lighten-3">Here's our small slogan.</h5>
             </div>
           </li>
           <li>
-            <img src="img/900x450.jpg"> <!-- random image -->
+            <img clss="responsive-img" src="img/p4.jpg"> <!-- random image -->
             <div class="caption center-align">
-              <h3 class="black-text">This is our big Tagline!</h3>
-              <h5 class="light black-text text-lighten-3">Here's our small slogan.</h5>
+              <h3 class="white-text">This is our big Tagline!</h3>
+              <h5 class="light white-text text-lighten-3">Here's our small slogan.</h5>
             </div>
           </li>
         </ul>
@@ -47,17 +47,17 @@
 
 
     <div class="row">
-      <div class="col s3 "><p><img class="materialboxed" src="img/cpu.jpg" width="300"></p></div>
-      <div class="col s3"><p><img class="materialboxed" src="img/case.jpg" width="300"></p></div>
-      <div class="col s3"><p><img class="materialboxed" src="img/fan.jpg" width="300"></p></div>
-      <div class="col s3"><p><img class="materialboxed" src="img/gc.jpg" width="300"></p></div>
+      <div class="col s3 "><p><img class="responsive-img" src="img/cpu.jpg" width="315"></p></div>
+      <div class="col s3"><p><img class="responsive-img" src="img/case.jpg" width="315"></p></div>
+      <div class="col s3"><p><img class="responsive-img" src="img/fan.jpg" width="315"></p></div>
+      <div class="col s3"><p><img class="responsive-img" src="img/gc.jpg" width="315"></p></div>
     </div>
 
     <div class="row">
-      <div class="col s3 "><p><img class="materialboxed" src="img/heat.jpg" width="300"></p></div>
-      <div class="col s3"><p><img class="materialboxed" src="img/mb.jpg" width="300"></p></div>
-      <div class="col s3"><p><img class="materialboxed" src="img/PS.jpg" width="300"></p></div>
-      <div class="col s3"><p><img class="materialboxed" src="img/ram.jpg" width="300"></p></div>
+      <div class="col s3 "><p><img class="responsive-img" src="img/heat.jpg" width="315"></p></div>
+      <div class="col s3"><p><img class="responsive-img" src="img/mb.jpg" width="315"></p></div>
+      <div class="col s3"><p><img class="responsive-img" src="img/PS.jpg" width="315"></p></div>
+      <div class="col s3"><p><img class="responsive-img" src="img/ram.jpg" width="315"></p></div>
     </div>
   </div>
 </div>
